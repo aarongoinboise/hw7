23579bb9e549f50b09a5b12ebe227324c663595f
Aaron Goin <aarongoin1990@gmail.com>
2023-04-02 10:46:58 -0600

    next date eq check

--- hw6/nextDateHandler.php
+++ hw6/nextDateHandler.php
@@ -7,12 +7,24 @@ $_SESSION['red'] = $_POST;
 $_SESSION['inputs'] = $_POST;
 $dao = new Dao();
 $email = $_SESSION['userEmail'];
+$type = $_SESSION['mType'];
 
 $date = $_POST['date'];
 if ($date != '') {
     unset($_SESSION['red']['date']);
 } else {
     err("next session date is blank", "Please make sure the date is filled out", 'homeM.php');
+}
+
+if ($type == 'tutor') {
+    $nextSesh = $dao->getNextSession($dao->getTutorNumber($email));
+} else {
+    $nextSesh = $dao->getNextSession($dao->getTutorNumber($dao->getTutorEmailFromStudent($email)));
+}
+
+if ($nextSesh == $date) {
+    $_SESSION['red']['date'] = 'set';
+    err("next session date is blank", "This date is already set!", 'homeM.php');
 }
 
 $dao->saveNextSesh($date, $dao->getTutorNumber($email));
